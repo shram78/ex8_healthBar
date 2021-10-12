3a4b7b4f3efe7e8909204dee28c72cf24a381530
shram78 <ra00275@gmail.com>
2021-10-12 22:16:30 +0300

    complete

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    private int _score; 
-  
-    private void ShowPointsInDebug()
+    [SerializeField] private int _health;
+
+    [SerializeField] private int _minHealth;
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _getDamageValue;
+    [SerializeField] private int _getHealthValue;
+
+    public event UnityAction<int, int> HealthChanged;
+
+    public void GetDamage()
     {
-        Debug.Log("Cобрано очков:" + _score);
+        ChangeHealth(_getDamageValue);
     }
 
-    public void AddScore() 
+    public void GetHealth()
     {
-        _score++;
-        ShowPointsInDebug();
+        ChangeHealth(_getHealthValue);
+    }
+
+    private void ChangeHealth(int healthValue)
+    {
+        _health += healthValue;
+
+        if (_health <= _minHealth)
+            _health = _minHealth;
+
+        if (_health >= _maxHealth)
+            _health = _maxHealth;
+
+        HealthChanged?.Invoke(_health, _maxHealth);
     }
 }
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnValueChenged;
+        _slider.value = 1;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnValueChenged;
+    }
+
+    public void OnValueChenged(int currentHealth, int maxHealth)
+    {
+        StartCoroutine(ChangeBar(currentHealth, maxHealth));
+    }
+
+    private IEnumerator ChangeBar(int currentHealth, int maxHealth)
+    {
+        do
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, (float)currentHealth / maxHealth, Time.deltaTime);
+            yield return null;
+        } while (_slider.value != (float)currentHealth / maxHealth);
+    }
+}
